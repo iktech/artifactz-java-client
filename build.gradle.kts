5e6de5df3385f7896a9742ff8afcd22f940f0668
Igor Kolomiyets <ikolomiyets@gmail.com>
2025-08-22 20:53:47 +0100

    Try a new Sonatype URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,13 +64,14 @@ publishing {
 
     repositories {
         maven {
+            name = "ossrh-staging-api"
             credentials {
                 username = System.getenv("MAVEN_USERNAME") ?: ossUsername ?: ""
                 password = System.getenv("MAVEN_PASSWORD") ?: ossPassword ?: ""
             }
 
-            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+            val releasesRepoUrl = "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://ossrh-staging-api.central.sonatype.com/content/repositories/snapshots/"
 
             url = uri(if (version.toString().endsWith("-SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
         }
